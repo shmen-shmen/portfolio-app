94f118c9592075c9b482ed577db424e1f1502158
shmen-shmen <akshevchenko@proton.me>
2023-06-13 19:53:10 +0600

    drum machie now works

--- client/src/features/drumMachine/drumMachine.js
+++ client/src/features/drumMachine/drumMachine.js
@@ -3,25 +3,44 @@ import { hide_drumMachine, padPress, padRelease } from "./drumMachineSlice";
 import "./drumMachine.css";
 
 const DrumMachine = () => {
-	const { display, pads } = useSelector((state) => state.drumMachine);
+	const { display, pads, nowPlaying } = useSelector(
+		(state) => state.drumMachine
+	);
 	const dispatch = useDispatch();
+
+	const handlePadPress = (e) => {
+		let padId;
+		e.code
+			? (padId = e.code.replace(/^Key/, ""))
+			: (padId = e.target.id.replace(/^pad-/, ""));
+
+		if (padId in pads) {
+			switch (e.type) {
+				case "keydown":
+				case "mousedown":
+					dispatch(padPress(padId));
+					document.getElementById(padId).play();
+					break;
+				case "keyup":
+					dispatch(padRelease(padId));
+					document.getElementById(padId).pause();
+					document.getElementById(padId).load();
+				case "mouseup":
+					dispatch(padRelease(padId));
+					break;
+				default:
+					break;
+			}
+		}
+	};
 
 	if (display) {
 		return (
 			<section
 				id="drum-machine"
 				tabIndex={0}
-				onKeyDown={(e) => {
-					dispatch(padPress(e.code));
-					const padId = e.code.replace(/^Key/, "");
-					document.getElementById(padId).play();
-				}}
-				onKeyUp={(e) => {
-					dispatch(padRelease(e.code));
-					const padId = e.code.replace(/^Key/, "");
-					document.getElementById(padId).pause();
-					document.getElementById(padId).load();
-				}}
+				onKeyDown={handlePadPress}
+				onKeyUp={handlePadPress}
 			>
 				<button
 					id="drums-close-btn"
@@ -34,30 +53,26 @@ const DrumMachine = () => {
 				</button>
 				<div className="pad-bank">
 					{Object.keys(pads).map((pad) => {
-						const status = pads[pad];
+						const status = pads[pad]["press"];
 						return (
 							<div
 								id={"pad-" + pad}
 								key={"pad-" + pad}
+								onMouseDown={handlePadPress}
+								onMouseUp={handlePadPress}
 								className={`drum-pad drum-pad-${status}`}
-								onMouseDown={() => {
-									dispatch(padPress(pad));
-								}}
-								onMouseUp={() => {
-									dispatch(padRelease(pad));
-								}}
 							>
 								{pad}
 								<audio
 									className="clip"
 									id={pad}
-									src="/sounds/427428__fthgurdy__muezzins-call-to-prayer-in-almaty-kazakhstan.wav"
+									src={`/sounds/pads/${pads[pad]["sample"]}.wav`}
 								></audio>
 							</div>
 						);
 					})}
 				</div>
-				<div id="display"></div>
+				<div id="display">{nowPlaying}</div>
 			</section>
 		);
 	} else {
--- client/src/features/drumMachine/drumMachineSlice.js
+++ client/src/features/drumMachine/drumMachineSlice.js
@@ -4,19 +4,53 @@ const initialState = {
 	displayName: "Drum Machine",
 	display: true,
 	pads: {
-		Q: false,
-		W: false,
-		E: false,
-		A: false,
-		S: false,
-		D: false,
-		Z: false,
-		X: false,
-		C: false,
+		Q: {
+			press: false,
+			sample: "HE",
+			emoji: "👋",
+		},
+		W: {
+			press: false,
+			sample: "BI",
+			emoji: "👴🏻",
+		},
+		E: {
+			press: false,
+			sample: "IZ",
+			emoji: "🧔🏻",
+		},
+		A: {
+			press: false,
+			sample: "WN",
+			emoji: "🙏",
+		},
+		S: {
+			press: false,
+			sample: "5B",
+			emoji: "5️⃣🅱️🚀",
+		},
+		D: {
+			press: false,
+			sample: "TB",
+			emoji: "💣🤯",
+		},
+		Z: {
+			press: false,
+			sample: "DO",
+			emoji: "🏬",
+		},
+		X: {
+			press: false,
+			sample: "CH",
+			emoji: "👶",
+		},
+		C: {
+			press: false,
+			sample: "SU",
+			emoji: "🔥🇺🇦🔥",
+		},
 	},
-	sounds: {
-		Q: "",
-	},
+	nowPlaying: "",
 };
 
 export const drumMachineSlice = createSlice({
@@ -30,15 +64,17 @@ export const drumMachineSlice = createSlice({
 			state.display = false;
 		},
 		padPress: (state, action) => {
-			const pad = action.payload.replace(/^Key/, "");
+			const pad = action.payload;
 			if (pad in state.pads) {
-				state.pads[pad] = true;
+				state.pads[pad].press = true;
+				state.nowPlaying = state.pads[pad]["emoji"];
 			}
 		},
 		padRelease: (state, action) => {
-			const pad = action.payload.replace(/^Key/, "");
+			const pad = action.payload;
 			if (pad in state.pads) {
-				state.pads[pad] = false;
+				state.pads[pad].press = false;
+				state.nowPlaying = "";
 			}
 		},
 	},
@@ -46,19 +82,5 @@ export const drumMachineSlice = createSlice({
 
 export const { show_drumMachine, hide_drumMachine, padPress, padRelease } =
 	drumMachineSlice.actions;
-
-// // The function below is called a selector and allows us to select a value from
-// // the state. Selectors can also be defined inline where they're used instead of
-// // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-// export const selectCount = (state) => state.counter.value;
-
-// // We can also write thunks by hand, which may contain both sync and async logic.
-// // Here's an example of conditionally dispatching actions based on current state.
-// export const incrementIfOdd = (amount) => (dispatch, getState) => {
-// 	const currentValue = selectCount(getState());
-// 	if (currentValue % 2 === 1) {
-// 		dispatch(incrementByAmount(amount));
-// 	}
-// };
 
 export default drumMachineSlice.reducer;
