df6691a49554a4d4fe7c990b1639710ac6aa00c9
shmen-shmen <akshevchenko@proton.me>
2023-06-19 20:06:10 +0600

    AC button functionality

--- client/src/features/calculator/calculator.js
+++ client/src/features/calculator/calculator.js
@@ -1,9 +1,9 @@
 import { useSelector, useDispatch } from "react-redux";
-import { hide_calculator } from "./calculatorSlice";
+import { hide_calculator, clear_calculator } from "./calculatorSlice";
 import "./calculator.scss";
 
 const Calculator = () => {
-	const { display, numbers, controls } = useSelector(
+	const { display, numbers, controls, output } = useSelector(
 		(state) => state.calculator
 	);
 	const dispatch = useDispatch();
@@ -12,19 +12,34 @@ const Calculator = () => {
 		dispatch(hide_calculator());
 	};
 
+	const handleControlPress = (e) => {
+		const { id } = e.target;
+		switch (id) {
+			case "AC":
+				dispatch(clear_calculator());
+				break;
+
+			default:
+				break;
+		}
+	};
+
 	return display ? (
 		<section id="calculator-container">
 			<div id="calculator">
 				<button className="calc-close-btn" onClick={handleCalcExit}>
 					X
 				</button>
-				<div id="display">66666666</div>
+				<div id="display">{output}</div>
 				<div id="buttons">
 					<div id="numpad">
 						{Object.keys(numbers).map((num) => {
-							console.log(num);
 							return (
-								<button id={num} className="calc-control-btn">
+								<button
+									id={num}
+									key={num + "-key"}
+									className="calc-control-btn"
+								>
 									{numbers[num]}
 								</button>
 							);
@@ -33,7 +48,12 @@ const Calculator = () => {
 					<div id="controls">
 						{Object.keys(controls).map((control) => {
 							return (
-								<button id={control} className="calc-control-btn">
+								<button
+									id={control}
+									key={control + "-key"}
+									className="calc-control-btn"
+									onClick={handleControlPress}
+								>
 									<span>{controls[control]}</span>
 								</button>
 							);
